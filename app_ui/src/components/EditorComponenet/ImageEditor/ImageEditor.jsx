79b27d82c3dd9fd9a3785a80082e7227cf6fbf8d
Shikher Shukla <shikher.shukla02@gmail.com>
2022-11-24 14:08:29 +0530

    add text Extraction login on editing page

--- app_ui/src/components/EditorComponenet/ImageEditor/ImageEditor.jsx
+++ app_ui/src/components/EditorComponenet/ImageEditor/ImageEditor.jsx
@@ -1,9 +1,11 @@
 import styles from "./ImageEditor.module.css";
+import TextExtraction from "../../testExtraction";
 
 const ImageEditor = () => {
   return (
     <div className={styles.ImageEditor}>
       <h1>Image Editor</h1>
+      <TextExtraction />
     </div>
   );
 };
