5b14d92151f96d988b521d30f3cda1dcf7ea749e
Sushant Mishra <70423224+sushantdev-git@users.noreply.github.com>
2022-11-25 07:07:09 +0530

    pdf to image to base64 string conversion

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -4,14 +4,12 @@ const authController = require("../controller/authenticationController");
 const userController = require("../controller/userController");
 const multiUpload = require("../controller/uploadimage")
 
-const multiUpload = require("../controller/uploadimage");
-
 const router = express.Router();
 
 router.post("/signup", authController.signUp);
 router.post("/login", authController.login);
 
 router.get("/", authController.verifyToken, userController.getUser);
-router.post("/addDocument", authController.verifyToken, multiUpload, userController.addDocument)
+router.post("/addDocument", userController.addDocument)
 
 module.exports = router;
--- app_ui/src/components/DropZone/DropZone.jsx
+++ app_ui/src/components/DropZone/DropZone.jsx
@@ -1,23 +1,32 @@
-import styles from './DropZone.module.css'
+import styles from "./DropZone.module.css";
 
 import { useCallback } from "react";
-import {useDropzone} from 'react-dropzone'
+import { useDropzone } from "react-dropzone";
 
-const DropZone = ({addFiles}) => {
-    let accept;
-    const onDrop = useCallback(addFiles,[]);
+const DropZone = ({ addFiles }) => {
+  let accept;
+  const onDrop = useCallback(addFiles, []);
 
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
-    return (
-        <div {...getRootProps()} className={styles.Container}>
-            <input {...getInputProps()} />
-            {
-                isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-        </div>
-    )
+  const accpetTypes  = {
+    "image/png": [".png", ".jpg"],
+    "application/pdf": [".pdf"],
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: accpetTypes,
+  });
+
+  return (
+    <div {...getRootProps()} className={styles.Container}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
 };
 
 export default DropZone;
